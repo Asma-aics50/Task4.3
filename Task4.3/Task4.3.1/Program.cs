5d0aa32c681480b0867ff665b762c7314856f864
Asma-aics50 <114606316+Asma-aics50@users.noreply.github.com>
2023-12-20 15:52:56 +0200

    Add files via upload

---
+++ Task4.3/Task4.3.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Task4._3._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<string> vowels = new List<string>() { "a", "e","i","o","u" };
+        static bool MYstring(string words, List<string> vowels)
+        {
+            foreach (char c in words.ToLower())
+            {
+                if (vowels.Contains(c.ToString()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        try
+        {
+
+            Console.WriteLine("Enter a word:");
+            string  words =  Console.ReadLine();
+            if (!MYstring(words, vowels))
+            {
+
+                throw new Exception("the word doesn't contains vowels");
+            }
+            Console.WriteLine($"the word {words} contains vowel ");
+
+        }
+
+        catch (Exception ex)
+        {
+
+            Console.WriteLine($"Error: {ex.Message}");
+
+        }
+    }
+}
+
---
+++ Task4.3/Task4.3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task4._3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> num = new List<int>();
+        try
+        {
+            
+            do
+            {
+
+                int num1 = '\0';
+               
+                Console.WriteLine("Enter the number in my list :");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                
+                if (num.Contains(num1))
+                {
+                     throw new Exception("Duplicate numbers found in the list.");
+                }
+                
+                num.Add(num1);
+
+                Console.WriteLine($"{num1} add the number  to list");
+            } while (true);
+           
+        }
+           
+      
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        
+    }
+}
+
